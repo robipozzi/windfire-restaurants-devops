281860b13184a5ff5d8d44b250e7c7aba6dac042
Roberto Pozzi <robertopozzi@Robertos-MBP.lan>
2020-03-31 22:06:50 +0200

    add AWS terraform templates

--- aws/network.tf
+++ aws/network.tf
@@ -1,81 +1,160 @@
 # Start - AWS VPC
-resource "aws_vpc" "robi-vpc" {
-  cidr_block            = "${var.vpcCIDRblock}"
-  instance_tenancy      = "${var.instanceTenancy}"
-  enable_dns_support    = "${var.dnsSupport}"
-  enable_dns_hostnames  = "${var.dnsHostNames}"
+resource "aws_vpc" "windfire-vpc" {
+  cidr_block = "${var.vpcCIDRblock}"
+  instance_tenancy = "${var.instanceTenancy}"
+  enable_dns_support = "${var.dnsSupport}"
+  enable_dns_hostnames = "${var.dnsHostNames}"
   tags = {
-    Name = "var.vpc['tag']"
+    Name = "${var.vpc["tags"]}"
   }
 }
-# Create the Subnet
-resource "aws_subnet" "robi-subnet1" {
-  vpc_id = "${aws_vpc.robi-vpc.id}"
-  cidr_block = "${var.subnetCIDRblock}"
+# Create the Subnets
+resource "aws_subnet" "windfire-frontend-subnet1" {
+  vpc_id = "${aws_vpc.windfire-vpc.id}"
+  cidr_block = "${var.frontendSubnetCIDRblock}"
   map_public_ip_on_launch = "${var.mapPublicIP}"
   availability_zone = "${var.availabilityZone}"
+  tags = {
+    Name = "${var.frontendSubnet["tags"]}"
+  }
+}
+resource "aws_subnet" "windfire-backend-subnet1" {
+  vpc_id = "${aws_vpc.windfire-vpc.id}"
+  cidr_block = "${var.backendSubnetCIDRblock}"
+  map_public_ip_on_launch = "${var.mapPublicIP}"
+  availability_zone = "${var.availabilityZone}"
+  tags = {
+    Name = "${var.backendSubnet["tags"]}"
+  }
 }
 # Create VPC Network access control list
-resource "aws_network_acl" "robi-vpc-acl" {
-  vpc_id = "${aws_vpc.robi-vpc.id}"
-  subnet_ids = [ "${aws_subnet.robi-subnet1.id}" ]
-  # allow port 22
+resource "aws_network_acl" "windfire-frontend-acl" {
+  vpc_id = "${aws_vpc.windfire-vpc.id}"
+  subnet_ids = [ "${aws_subnet.windfire-frontend-subnet1.id}" ]
+  # allow HTTP port 80
   ingress {
     protocol   = "tcp"
     rule_no    = 100
     action     = "allow"
-    cidr_block = "${var.destinationCIDRblock}"
-    from_port  = 22
-    to_port    = 22
+    cidr_block = "${var.allIPsCIDRblock}"
+    from_port  = 80
+    to_port    = 80
   }
-  # allow ingress ephemeral ports 
+  # allow SSH port 22
   ingress {
     protocol   = "tcp"
     rule_no    = 200
     action     = "allow"
-    cidr_block = "${var.destinationCIDRblock}"
-    from_port  = 1024
-    to_port    = 65535
+    cidr_block = "${var.allIPsCIDRblock}"
+    from_port  = 22
+    to_port    = 22
   }
   # allow egress ephemeral ports
   egress {
     protocol   = "tcp"
     rule_no    = 100
     action     = "allow"
-    cidr_block = "${var.destinationCIDRblock}"
+    cidr_block = "${var.allIPsCIDRblock}"
     from_port  = 1024
     to_port    = 65535
   }
+  tags = {
+    Name = "${var.vpc-acl["tags"]}"
+  }
+}
+resource "aws_network_acl" "windfire-backend-acl" {
+  vpc_id = "${aws_vpc.windfire-vpc.id}"
+  subnet_ids = [ "${aws_subnet.windfire-backend-subnet1.id}" ]
+  # allow HTTP port 8080
+  ingress {
+    protocol   = "tcp"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "${var.vpcCIDRblock}"
+    from_port  = 8080
+    to_port    = 8080
+  }
+  # allow SSH port 22
+  ingress {
+    protocol   = "tcp"
+    rule_no    = 200
+    action     = "allow"
+    cidr_block = "${var.allIPsCIDRblock}"
+    from_port  = 22
+    to_port    = 22
+  }
+  # allow egress ephemeral ports
+  egress {
+    protocol   = "tcp"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "${var.allIPsCIDRblock}"
+    from_port  = 1024
+    to_port    = 65535
+  }
+  tags = {
+    Name = "${var.vpc-acl["tags"]}"
+  }
 }
 # Create the Security Group
-resource "aws_security_group" "robi-vpc-security-group" {
-  vpc_id       = "${aws_vpc.robi-vpc.id}"
-  name = "var.security-group['name']"
-  description = "var.security-group['description']"
+resource "aws_security_group" "windfire-frontend-securitygroup" {
+  vpc_id       = "${aws_vpc.windfire-vpc.id}"
+  name         = "${var.frontend-security-group["name"]}"
+  description  = "${var.-frontendsecurity-group["description"]}"
+  # allow HTTP port 80
+  ingress {
+    cidr_blocks = "${var.ingressCIDRblock}"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+  }
+  # allow SSH port 22
   ingress {
     cidr_blocks = "${var.ingressCIDRblock}"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
   }
+  # allow egress ephemeral ports
+  egress {
+    cidr_block = "${var.allIPsCIDRblock}"
+    from_port  = 1024
+    to_port    = 65535
+    protocol   = "tcp"
+  }
+  tags = {
+    Name = "${var.security-group["tags"]}"
+  }
 }
 # Create the Internet Gateway
-resource "aws_internet_gateway" "robi-vpc-gw" {
-  vpc_id = "${aws_vpc.robi-vpc.id}"
+resource "aws_internet_gateway" "windfire-igw" {
+  vpc_id = "${aws_vpc.windfire-vpc.id}"
+  tags = {
+    Name = "${var.internet-gateway["tags"]}"
+  }
 }
-# Create the Route Table
-resource "aws_route_table" "robi-vpc-route-table" {
-  vpc_id = "${aws_vpc.robi-vpc.id}"
+# Create Route Tables
+resource "aws_route_table" "windfire-public-routetable" {
+  vpc_id = "${aws_vpc.windfire-vpc.id}"
+  tags = {
+    Name = "${var.public-routetable["tags"]}"
+  }
+}
+resource "aws_route_table" "windfire-private-routetable" {
+  vpc_id = "${aws_vpc.windfire-vpc.id}"
+  tags = {
+    Name = "${var.public-routetable["tags"]}"
+  }
 }
 # Create the Internet Access
-resource "aws_route" "My_VPC_internet_access" {
-  route_table_id         = "${aws_route_table.robi-vpc-route-table.id}"
-  destination_cidr_block = "${var.destinationCIDRblock}"
-  gateway_id             = "${aws_internet_gateway.robi-vpc-gw.id}"
+resource "aws_route" "windfire-internet-access" {
+  route_table_id         = "${aws_route_table.windfire-public-routetable.id}"
+  destination_cidr_block = "${var.allIPsCIDRblock}"
+  gateway_id             = "${aws_internet_gateway.windfire-igw.id}"
 }
 # Associate the Route Table with the Subnet
 resource "aws_route_table_association" "My_VPC_association" {
-  subnet_id      = "${aws_subnet.robi-subnet1.id}"
-  route_table_id = "${aws_route_table.robi-vpc-route-table.id}"
+  subnet_id      = "${aws_subnet.windfire-frontend-subnet1.id}"
+  route_table_id = "${aws_route_table.windfire-public-routetable.id}"
 }
 # End - AWS VPC
--- aws/variables.tf
+++ aws/variables.tf
@@ -2,7 +2,7 @@ variable "aws_access_key" { }
 variable "aws_secret_key" { }
 variable "aws_region" {
   description = "One of us-east-2, us-east-1, us-west-1, us-west-2, ap-south-1, ap-northeast-2, ap-southeast-1, ap-southeast-2, ap-northeast-1, us-west-2, eu-central-1, eu-west-1, eu-west-2, eu-west-2, eu-north-1, sa-east-1"
-  default     = "eu-central-1"
+  default  = "eu-central-1"
 }
 variable "availabilityZone" {
   default = "eu-central-1a"
@@ -10,33 +10,39 @@ variable "availabilityZone" {
 # AWS Network configuration variables - start
 variable "vpc" {
   description = "Virtual Private Cloud definition"
-  #default     = "robi-vpc"
-  default = {
-    "name" = "robi-vpc"
-    "tag" = "Robi VPC"
-  }
+  default = "windfire-vpc"
+  #tags     = "Windfire VPC"
 }
-variable "subnet" {
-  default = "robi-subnet1"
+variable "frontendSubnet" {
+  default = "windfire-frontend-subnet1"
+  #tags    = "Windfire frontend subnet 1"
 }
-variable "security-group" {
-  description = "Robi VPC Security Group"
-  default     = "robi-vpc-security-group"
+variable "backendSubnet" {
+  default = "windfire-backend-subnet1"
+  #tags    = "Windfire backend subnet 1"
 }
 variable "vpc-acl" {
-  default = "robi-vpc-acl"
+  default = "windfire-acl"
+  #tags    = "Windfire ACL"
+}
+variable "frontend-security-group" {
+  description = "Frontend Security Group"
+  default     = "windfire-securitygroup"
+  #tags        = "Windfire Security Group"
 }
 variable "internet-gateway" {
-  default = "robi-vpc-gw"
+  default = "windfire-igw"
+  #tags    = "Windfire Internet Gateway"
 }
-variable "route-table" {
-  default = "robi-vpc-route-table"
+variable "public-routetable" {
+  default = "windfire-routetable"
+  #tags    = "Windfire Route Table"
 }
 variable "instanceTenancy" {
-  default = "default"
+ default = "default"
 }
 variable "dnsSupport" {
-  default = true
+ default = true
 }
 variable "dnsHostNames" {
   default = true
@@ -44,10 +50,13 @@ variable "dnsHostNames" {
 variable "vpcCIDRblock" {
   default = "10.0.0.0/16"
 }
-variable "subnetCIDRblock" {
+variable "frontendSubnetCIDRblock" {
   default = "10.0.1.0/24"
 }
-variable "destinationCIDRblock" {
+variable "backendSubnetCIDRblock" {
+  default = "10.0.2.0/24"
+}
+variable "allIPsCIDRblock" {
   default = "0.0.0.0/0"
 }
 variable "ingressCIDRblock" {
@@ -58,11 +67,11 @@ variable "mapPublicIP" {
 }
 # AWS Network configuration variables - end
 variable "ssh_user" {
-  default     = "ec2-user"
+  default = "ec2-user"
   description = "Most Ubuntu AMIs use Ubuntu as the default user. Normally safe to leave"
 }
 variable "bootstrap_script" {
-  default     = "bootstrap.sh"
+  default = "bootstrap.sh"
   description = ""
 }
 variable "bootstrap_script_dest_dir" {
