b3f4cc4b9f1a953273141fa2eab31024d6d84562
Roberto Pozzi <robertopozzi@Robertos-MBP.lan>
2020-04-24 13:59:44 +0200

    add output for Backend instance private ip

--- aws/output.tf
+++ aws/output.tf
@@ -4,3 +4,6 @@ output "bastion-public_ip" {
 output "bastion-public_dns" {
   value = "${aws_instance.bastion.public_dns}"
 }
+output "backend-private_ip" {
+  value = "${aws_instance.windfire-backend.private_ip}"
+}
