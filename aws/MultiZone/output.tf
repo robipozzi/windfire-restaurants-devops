d7ead20bf3a6698b0bb5624ff4ad997a71856a62
Roberto Pozzi <robertopozzi@Robertos-MBP.lan>
2020-05-17 10:39:05 +0200

    update Terraform output

--- aws/MultiZone/output.tf
+++ aws/MultiZone/output.tf
@@ -4,6 +4,6 @@ output "bastion-public_ip" {
 output "bastion-public_dns" {
   value = "${aws_instance.bastion.public_dns}"
 }
-/*output "backend-private_ip" {
-  value = "${aws_instance.windfire-backend.private_ip}"
-}*/
+output "alb-public_dns" {
+  value = "${aws_lb.windfire-frontend-alb.dns_name}"
+}
